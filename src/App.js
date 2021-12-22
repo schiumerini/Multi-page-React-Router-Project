4f6eaee77bc5f1ce66005eceae867568e59a7863
D10S <dschi002@fiu.edu>
2021-12-22 18:51:35 -0500

    Adds practice Routing, useParams, Links and Redirecting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Products from "./components/pages/Products";
+import Welcome from "./components/pages/Welcome";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./components/pages/ProductDetail";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
